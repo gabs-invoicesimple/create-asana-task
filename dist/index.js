9733bfb5486a7ce4be7764f3fd1f60b084224d34
Gabs <gabs@gabs.work>
2022-05-04 09:52:35 -0700

    Fixes on action and dist

--- dist/index.js
+++ dist/index.js
@@ -66,6 +66,7 @@ function run() {
             core.setOutput('result', 'Completed!');
         }
         catch (error) {
+            core.debug(JSON.stringify(error, null, 2));
             if (error instanceof Error)
                 core.setFailed(error.message);
         }
