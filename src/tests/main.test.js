fdf648a2bdee52b3ae78e565db240afadbcc6811
Gabs <gabs@gabs.work>
2022-05-03 21:24:08 -0700

    Dist for initial version

--- src/tests/main.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const globals_1 = require("@jest/globals");
-// shows how the runner will run a javascript action with env / stdout protocol
-(0, globals_1.test)('test runs', () => {
-    (0, globals_1.expect)(true).toBeTruthy();
-});
